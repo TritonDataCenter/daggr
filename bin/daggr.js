b360b8261cbdf0ff6a60a3b04e6a2eb8aa48f936
Dave Pacheco <dap@joyent.com>
2013-08-23 17:38:32 -0700

    add usage message

--- bin/daggr.js
+++ bin/daggr.js
@@ -11,8 +11,39 @@ var sprintf = mod_extsprintf.sprintf;
 
 var daArg0 = mod_path.basename(process.argv[1]);
 var daUsage = sprintf([
-    'usage: %s ACTION'
-].join('\n'), daArg0);
+    'usage: %s [-j] [-k FIELD...] [-f FILTER ...] [-o FIELD...] [-v FIELD] ' +
+        '[ACTION]',
+    '',
+    '%s takes an input stream of plaintext or JSON data and filters it, ',
+    'selects certain fields, or aggregates numeric values, optionally grouped ',
+    'by a different field.',
+    '',
+    'Input is assumed to be plaintext by default.  Use -j for JSON data.',
+    '',
+    'FILTER is a JavaScript expression that will be evaluated for each input ',
+    'row.  If the result is not truthy, the row is discarded.  For plaintext ',
+    '',
+    'FIELD is a JavaScript propety expression, as in "prop1.prop2[...]".  This ',
+    'may not be an arbitrary expression, and if the property is not found, ',
+    'the result is the JavaScript undefined value.',
+    '',
+    'The default action is to print the input directly.  In this mode, you ',
+    'can filter rows with -f and specify which fields to print with -o.',
+    '',
+    'Several aggregations are available, which result in no output until ',
+    'the input has been fully read, at which point the aggregated result ',
+    'is printed.  Available aggregations include:',
+    '',
+    '    avg',
+    '    count',
+    '    max',
+    '    min',
+    '    sum',
+    '',
+    'With these aggregations, you can use -k to specify another field to ',
+    'group the results by.  By default, the value in column 1 is used for ',
+    'the aggregation, but you can change this with -v.',
+].join('\n'), daArg0, daArg0);
 
 function main()
 {
@@ -59,7 +90,7 @@ function main()
 		action = process.argv[parser.optind()];
 
 	if (outputs === null)
-		outputs = [ '0' ];
+		outputs = [ mode == 'json' ? 'this' : '0' ];
 
 	source = process.stdin;
 	stream = new mod_daggr.RowStream({
